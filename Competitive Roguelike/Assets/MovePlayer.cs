cc2f39d4b3b2f120891e38410a57e3c192cde04c
ADS\jdriggin <jdriggin@umail.iu.edu>
2018-08-28 12:32:10 -0400

    Adds pbasic layer movement.

---
+++ Competitive Roguelike/Assets/MovePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        
+        //rb.AddForce(movement * speed);
+
+        this.GetComponent<Rigidbody2D>().transform.Translate(movement * speed);
+	}
+}
