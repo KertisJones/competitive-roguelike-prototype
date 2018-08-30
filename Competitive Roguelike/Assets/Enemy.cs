aaa232e2ab11b09ec363621048c53e18aac9c0db
Jesse Riggins <jesse.riggins98@gmail.com>
2018-08-30 01:50:54 -0400

    Adds enemy. Adds enemy spawning. Enemy moves towards player.

---
+++ Competitive Roguelike/Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    // The target marker.
+    private Transform target;
+
+    // Speed in units per sec.
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        // Move our position a step closer to the target.
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
--- Competitive Roguelike/Assets/spawnEnemy.cs
+++ Competitive Roguelike/Assets/spawnEnemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class spawnEnemy : MonoBehaviour {
+    public GameObject wall;
     public GameObject enemy;
 
 	// Use this for initialization
@@ -14,9 +15,15 @@ public class spawnEnemy : MonoBehaviour {
 	void Update () {
         if (Input.GetButtonDown("Fire1"))
         {
-            Debug.Log("Trying to spawn...");
+            //Debug.Log("Trying to spawn...");
             Vector3 spawnPoint = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
             Instantiate(enemy, spawnPoint, transform.rotation);
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            //Debug.Log("Trying to spawn...");
+            Vector3 spawnPoint = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            Instantiate(wall, spawnPoint, transform.rotation);
         }
     }
 }
