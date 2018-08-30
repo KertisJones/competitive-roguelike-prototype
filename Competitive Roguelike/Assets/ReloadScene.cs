ca3c7ac307d501f752e4359f75f248282443f70f
Jesse Riggins <jesse.riggins98@gmail.com>
2018-08-30 02:02:00 -0400

    Touching an enemy reloads scene. Reaching the end reloads the scene.

---
+++ Competitive Roguelike/Assets/ReloadScene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour {
+    public float timeLeft = 0f;
+
+    private bool startTimer = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTimer)
+        {
+            if (timeLeft >= 0)
+            {
+                timeLeft -= Time.deltaTime;
+
+                if (timeLeft < 0)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            startTimer = true;
+        }
+    }
+}
